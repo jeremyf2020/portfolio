c85adf0126b27df48b2ebff1052070dd3f558ead
jeremyf2020 <jeremy2020fan@gmail.com>
2024-06-26 02:33:50 +0100

    update nav bar

---
+++ src/layouts/NavBar.tsx
@@ -0,0 +1,21 @@
+export default function NavBar() {
+  return (
+    <div className="py-6 flex justify-between mb-10">
+      <div
+        className="flex items-center bg-gradient-to-br from-sky-700 to-cyan-100 
+            bg-clip-text text-xl font-bold text-transparent"
+      >
+        {/* <SquareMenu className="mr-1 h-10 w-10 stroke-cyan-600" /> */}
+        <div>Jeremy's Portfolio</div>
+      </div>
+      <ul className="flex flex-row gap-x-8 font-medium">
+        <li className="hover:text-amber-200 cursor-pointer">About me</li>
+        <li className="hover:text-amber-200 cursor-pointer">Projects</li>
+        <li className="hover:text-amber-200 cursor-pointer">Skills</li>
+        <li className="hover:text-amber-200 cursor-pointer">Education</li>
+        <li className="hover:text-amber-200 cursor-pointer">Experience</li>
+        <li className="hover:text-amber-200 cursor-pointer">Contact</li>
+      </ul>
+    </div>
+  );
+}
